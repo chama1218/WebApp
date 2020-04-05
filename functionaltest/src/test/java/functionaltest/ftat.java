335941b26b58f8481e3692125bec085425df80e8
chama1218 <32400737+chama1218@users.noreply.github.com>
2020-04-05 15:37:05 -0400

    Update ftat.java

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -99,7 +99,8 @@ public class ftat {
 
 	@Test(priority = 4)
 	public void logouttest() {
-		driver.findElement(By.partialLinkText("Logout")).click();
+		//driver.findElement(By.partialLinkText("Logout")).click();
+		driver.findElement(By.xpath("//button[text()='Logout']")).click();
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		driver.findElement(By.xpath("//h1[text()='MARS COMMUNICATION']")).isDisplayed();
 		driver.findElement(By.name("username")).sendKeys("guest");
